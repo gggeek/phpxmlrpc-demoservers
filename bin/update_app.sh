84eed964a1153746c7bdfc0c97e080a969e0220e
gggeek <giunta.gaetano@gmail.com>
2025-11-07 23:23:26 +0000

    improve update script

--- bin/update_app.sh
+++ bin/update_app.sh
@@ -5,6 +5,6 @@ set -e
 cd "$(dirname -- "$(dirname -- "$(readlink -f "$0")")")"
 
 # this is needed, or the update would fail because of missing files
-COMPOSER_DISCARD_CHANGES=true  composer install --no-interaction
+COMPOSER_DISCARD_CHANGES=true composer install --no-scripts --no-interaction
 
 composer update --prefer-source --with-all-dependencies --optimize-autoloader --no-interaction
