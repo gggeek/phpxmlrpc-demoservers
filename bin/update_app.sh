bcaa72a3e1d81af593f9e91d4289f73359dfce9c
gggeek <giunta.gaetano@gmail.com>
2025-11-10 18:36:18 +0000

    try to improve the update script, more

--- bin/update_app.sh
+++ bin/update_app.sh
@@ -7,8 +7,9 @@ cd "$(dirname -- "$(dirname -- "$(readlink -f "$0")")")"
 export COMPOSER_DISCARD_CHANGES=true
 
 # this is needed, or the update would fail because of missing files
-composer install --no-scripts --no-interaction
+composer install --no-autoloader --no-scripts --no-interaction
 
 composer update --prefer-source --with-all-dependencies --no-autoloader --no-interaction
 
+# only dump the autoloader after having culled the files
 composer dump-autoload --optimize
